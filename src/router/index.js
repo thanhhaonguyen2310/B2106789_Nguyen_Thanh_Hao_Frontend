c26093e3298e30cbd24e22ed235bace86c215648
thanhhaonguyen2310 <threshazir123@gmail.com>
2025-07-24 21:04:02 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,13 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
+
 ];
 
 const router = createRouter({
